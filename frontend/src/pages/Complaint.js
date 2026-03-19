7a435068961360ede0c2b335e073bf504f67647c
Sneha Singh <snehasingh240903@gmail.com>
2026-03-19 18:11:12 +0530

    fix complaint path properly

--- frontend/src/pages/Complaint.js
+++ frontend/src/pages/Complaint.js
@@ -1,22 +1,3 @@
-import { useState } from "react";
-import axios from "axios";
-
-const API = process.env.REACT_APP_API;
-
 export default function Complaint() {
-  const [msg, setMsg] = useState("");
-  const token = localStorage.getItem("token");
-
-  return (
-    <>
-      <input onChange={e => setMsg(e.target.value)} />
-      <button onClick={async () => {
-        await axios.post(`${API}/api/complaints`,
-          { message: msg },
-          { headers: { Authorization: `Bearer ${token}` } });
-      }}>
-        Send
-      </button>
-    </>
-  );
+  return <h1>Complaint Page Working</h1>;
 }
