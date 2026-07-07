9889dcf2e4a31ace24fd47317b80f2c0e6f0b5ea
Sneha Singh <snehasingh240903@gmail.com>
2026-07-07 19:57:28 +0530

    Remove unused User icon

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,6 @@ import { useEffect, useMemo, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import {
-  User,
   Lock,
   Eye,
   EyeOff,
